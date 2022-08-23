278fa4620b11428d7ee37fcbaa4dcae65418c8be
nasim <nasimz@metropolia.fi>
2022-08-23 15:21:19 +0300

    uus

--- Hei.py
+++ Hei.py
@@ -1,4 +1,30 @@
 
 
-print("Hei maailma")
+#print("Hei Maailma")
+
+
+#print('"hei", sanoi Ville')
+
+#print("hyvää")
+#print('huomenta')
+
+
+#käyttäjä = input('nasim:')
+#print("Hauska tavata, " + käyttäjä + "!")
+
+eka = -9
+toka = 12456123180
+kolmas = 4.973
+neljäs = -4 + 2j
+
+print(eka)
+print(toka)
+print(kolmas)
+print(neljäs)
+print(neljäs.real)
+print(neljäs.imag)
+
+
+
+
 
