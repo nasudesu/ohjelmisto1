b2c5934d540092b49fd0fd8246c85cc0e5a2ba61
nasim <nasimz@metropolia.fi>
2022-08-23 15:41:29 +0300

    123

--- Hei.py
+++ Hei.py
@@ -12,19 +12,4 @@
 #käyttäjä = input('nasim:')
 #print("Hauska tavata, " + käyttäjä + "!")
 
-eka = -9
-toka = 12456123180
-kolmas = 4.973
-neljäs = -4 + 2j
-
-print(eka)
-print(toka)
-print(kolmas)
-print(neljäs)
-print(neljäs.real)
-print(neljäs.imag)
-
-
-
-
-
+print('nasim')
