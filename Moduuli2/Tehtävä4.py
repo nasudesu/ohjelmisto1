af91a69028e9242b1487f1ec44f12c640132fd59
nasudesu <nasimz@metropolia.fi>
2022-08-24 16:26:29 +0300

    Moduuli2 th4 tehty

---
+++ Moduuli2/Tehtävä4.py
@@ -0,0 +1,11 @@
+
+luku1_str = input('Anna luku1: ')
+luku2_str = input('Anna luku2: ')
+luku3_str = input('Anna luku3: ')
+num1 = int(luku1_str)
+num2 = int(luku2_str)
+num3 = int(luku3_str)
+print('Lukujen summa: ' + str(num1+num2+num3))
+print('Lukujen erotus: ' + str(num1-num2-num3))
+print('Lukujen keksiarvo: ' + str((num1+num2+num3)/3))
+
