fc0d91aad2cd7c97d3463932a80219b32e8d80b3
nasudesu <nasimz@metropolia.fi>
2022-08-31 23:32:18 +0300

    M4 t1

---
+++ Moduuli3/Tehtava4.py
@@ -0,0 +1,5 @@
+yer = int(input("Anna vuosi: "))
+if yer % 4 == 0:
+    print("on karkausvuosi")
+else:
+    print("ei ole karkausvuosi")
---
+++ Moduuli4/Tehtava1.py
@@ -0,0 +1,6 @@
+
+nolla = 0
+while nolla < 1000:
+ print("Lisää (3): " + str(nolla))
+ nolla = nolla + 3
+
