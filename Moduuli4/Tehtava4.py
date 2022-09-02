b6cd6effef03c8507576cc96e533167957e7c0f1
nasim <nasimz@metropolia.fi>
2022-09-02 11:31:37 +0300

    korjattu

--- Moduuli4/Tehtava4.py
+++ Moduuli4/Tehtava4.py
@@ -1,10 +1,9 @@
 import random
 luku1 = random.randint(1, 10)
-numero = int(input("Arvaa numero"))
 while True:
+     numero = int(input("Arvaa numero"))
      if numero < luku1:
         print("Liian pieni arvaus")
-        numero = int(input("Arvaa numero"))
      elif numero > luku1:
          print("Liian suuri arvaus")
          numero = int(input("Arvaa numero"))
