0c0abb17be161841633d3d818014941acb8b4b97
nasudesu <nasimz@metropolia.fi>
2022-10-06 20:50:05 +0300

    ooo

---
+++ Moduuli8/Tehtava2_harjotus.py
@@ -0,0 +1,26 @@
+import mysql.connector
+
+connection = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    database='flight_game',
+    user='root',
+    password='ohjelmisto1',
+    autocommit=True
+)
+
+def maatunnus_haku(FI):
+    sql = " SELECT type, count(*)" \
+          " FROM airport" \
+          " WHERE airport.iso_country = '" + FI + "'" \
+          " GROUP BY type"
+    print(sql)
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    for rivi in result:
+        print(f"Kenttä tyyppi: {rivi[0]}."
+              f" Näitten määrä: {rivi[1]}.")
+
+lol = input("Maatunnus: ")
+maatunnus_haku(lol)
--- Moduuli8/Tehtava3.py
+++ Moduuli8/Tehtava3.py
@@ -1,11 +1,44 @@
 import mysql.connector
+from geopy.distance import geodesic
 
-yhteys = mysql.connector.connect(
-         host='127.0.0.1',
-         port= 3306,
-         database='flight_game',
-         user='root',
-         password='ohjelmisto1',
-         autocommit=True
-         )
+connection = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    database='flight_game',
+    user='root',
+    password='ohjelmisto1',
+    autocommit=True
+)
+def location1(code):
+    global point1
+    sql = "SELECT latitude_deg, longitude_deg " \
+          " FROM airport" \
+          " WHERE ident = '" + code + "'"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    for rivi in result:
+        point1 = []
+        point1.append(rivi[0])
+        point1.append(rivi[1])
+    return point1
 
+code1 = input("Anna ICAO-koodi1: ")
+def location2(code):
+    global point2
+    sql = "SELECT latitude_deg, longitude_deg " \
+          " FROM airport" \
+          " WHERE ident = '" + code + "'"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    for rivi in result:
+        point2 = []
+        point2.append(rivi[0])
+        point2.append(rivi[1])
+    return point2
+
+code2 = input("Anna ICAO-koodi2: ")
+
+print(f"Maitten välinen etäisyys: "
+      f"{geodesic(location1(code1), location2(code2)).km}km.")
