61d08e5d983dd4177aef627526d77e37c821e3a0
nasudesu <nasimz@metropolia.fi>
2022-11-04 01:17:49 +0200

    ooo

---
+++ Moduuli12/Tehtava1.py
@@ -0,0 +1,13 @@
+import requests
+
+request = "https://api.chucknorris.io/jokes/random"
+answer = requests.get(request).json()
+
+
+try:
+    answer = requests.get(request)
+    if answer.status_code==200:
+        json_answer = answer.json()
+        print(json_answer["value"])
+except requests.exceptions.RequestException as e:
+    print ("Invalid request.")
