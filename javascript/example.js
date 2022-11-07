609b9d3f3d6cc545e62e786d11dcc84c6e01dba6
nasim <nasimz@metropolia.fi>
2022-11-07 19:42:21 +0200

    hhh

---
+++ javascript/example.js
@@ -0,0 +1,22 @@
+'use strict';
+
+//console.log('This is text is printed to the console.');
+
+//alert('Hi from here too!');
+
+//let ikaint = 23;
+
+//let ikastr = ikaint.toString()
+
+//ikastr = ikastr+10;
+//ikaint = ikaint+10;
+
+//let ikastr = "23"
+//let ikaint = parseInt(ikastr)
+
+//document.querySelector("#target").innerHTML = ikastr;
+//document.querySelector("#target1").innerHTML = ikaint;
+
+
+let nimi = prompt("Anna nimesi");
+document.querySelector("#target").innerHTML = "Moi "+nimi+"!";
