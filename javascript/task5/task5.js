d31599c950f57211e1884bd6901dc896b3eddf00
nasudesu <nasimz@metropolia.fi>
2022-11-08 19:12:06 +0200

    ooo

--- javascript/task5/task5.js
+++ javascript/task5/task5.js
@@ -3,7 +3,7 @@
 const number = parseInt(prompt('Gimmy number'))
 if (0 === number % 4 && (0 !==  number % 100 || 0 === number % 400)) {
   console.log('knjnjn')
-  document.querySelector('#target').textContent = "Vuosi on karkaus vuosi" + number;
+  document.querySelector('#target').textContent = "Vuosi on karkaus vuos" + number;
 } else {
-  document.querySelector('#target').textContent = "Vuosi ei ole karrkaus vuosi" + number;
+  document.querySelector('#target').textContent = "Vuosi ei ole karrkaus vuos" + number;
 }
