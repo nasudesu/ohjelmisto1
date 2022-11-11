f625e14bbcd85304f7fe1197f142af89cff4617c
nasim <nasimz@metropolia.fi>
2022-11-11 09:47:12 +0200

    hhh

--- javascript/JavaModule1/task8/task8.js
+++ javascript/JavaModule1/task8/task8.js
@@ -1,19 +1,19 @@
 
 
 
-
-
-
-
 let start = parseInt(prompt(`Give me start yer`))
 let end = parseInt(prompt(`Give me end yer`))
 
-let times = end - start
-for (let i = 0; i <= times ; i++) {
-  let yearling = start+i
+//let times = end - start
+
+for (let i = start; i <= end ; i++) {
+  //  let yearling = start+i
+
   const node = document.createElement("li");
-  const textnodes = document.createTextNode(yearling);
-  node.appendChild(textnodes);
-  if (0 === yearling % 4 && (0 !==  yearling % 100 || 0 === yearling % 400)) {
-    document.getElementById("target").appendChild(node)
+  node.innerText = i;
+  //const textnodes = document.createTextNode(i);
+  //node.appendChild(textnodes);
+
+  if (0 === i % 4 && (0 !==  i % 100 || 0 === i % 400)) {
+    document.getElementById("target").appendChild(node);
 }}
